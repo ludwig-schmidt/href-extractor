8fd47a433b5abc821d9c06adf07d4d5837da57bd
Ludwig Schmidt <mail@ludwigschmidt.berlin>
2018-09-06 10:31:33 +0200

    style: js cleanup and remove .sass-cache

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
-const formInput = document.querySelector('.form__input'),
-	  formMessage = document.querySelector(".form__message");
-
-
-const handleCopy = input => {
-	let matches = input.value.match(/(?:href=")([^\'\"]+)/);
-	console.log(matches);
-	
-	if(!matches) {
-		formMessage.innerHTML = "No links found!";
-		return;
-	}
-	input.value = matches[1];
-
-	input.select();
-	document.execCommand('copy');
-	formMessage.innerHTML = "Copied to clipboard";
+const showFormMessage = message => {
+	document.querySelector(".form__message").innerHTML = message;
 }
 
-
 document.querySelector('.form__submit').addEventListener('click', e => {
+	let input = document.querySelector('.form__input');
+
+	// stop form from submitting
 	e.preventDefault();
 
-	handleCopy(formInput);
-});
+	// match regex against string in form
+	let matches = input.value.match(/(?:href=")([^\'\"]+)/);
 
+	// display error message when no link was found and stop execution
+	if(!matches) {
+		showFormMessage("No links found!");
+		return;
+	}
+	
+	// insert extracted link into textarea
+	input.value = matches[1];
+
+	// copy generated link to clipboard and display success message
+	input.select();
+	document.execCommand('copy');
+	showFormMessage("Copied to clipboard");
+});
